b60e3f4d9ee00d63b55a6b7cddc8ddc545cdd281
tusharpamnani <tusharpamnani1@gmail.com>
2025-07-21 19:57:37 +0000

    build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: ["@typescript-eslint"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error"
+    },
+  },
 ];
 
 export default eslintConfig;
